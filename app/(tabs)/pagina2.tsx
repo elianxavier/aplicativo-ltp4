3b6b04a75c771c71bea1f4f01f4a4b4b49ca4eb4
Elian Xavier <45861861+elianxavier@users.noreply.github.com>
2025-02-26 20:04:13 -0300

    Configuração inicial do projeto

---
+++ app/(tabs)/pagina2.tsx
@@ -0,0 +1,11 @@
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+import { View } from "react-native";
+
+export default function Pagina2 () {
+    return (
+        <ThemedView>
+            <Link href={"/"}>Teste</Link>
+        </ThemedView>
+    );
+}
